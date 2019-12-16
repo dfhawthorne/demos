6f38d2d999d1d677a2b1f60d65e21725ffb6d222
Douglas Hawthorne <dfhawthorne@computer.org>
2019-12-16 19:12:47 +1100

    Update create_jar_cdb_plan.sql
    Corrected comment
    Removed dropping of resource manager plan

--- high_cpu_and_cdb_resource_plans/create_jar_cdb_plan.sql
+++ high_cpu_and_cdb_resource_plans/create_jar_cdb_plan.sql
@@ -1,23 +1,9 @@
 -- -----------------------------------------------------------------------------------------------------------------------------
--- <a href="https://docs.oracle.com/database/121/ADMIN/cdb_dbrm.htm">44 Using Oracle Resource Manager for PDBs with SQL*Plus</a>
--- <a href="https://docs.oracle.com/database/121/ADMIN/cdb_dbrm.htm#ADMIN-GUID-36E58D2B-2873-4ED1-B042-624DB42A5C8E">44.3.1 Creating a CDB Resource Plan: A Scenario</a>
+-- See https://docs.oracle.com/database/121/ADMIN/cdb_dbrm.htm#ADMIN-GUID-36E58D2B-2873-4ED1-B042-624DB42A5C8E
+-- 44.3.1 Creating a CDB Resource Plan: A Scenario
 -- -----------------------------------------------------------------------------------------------------------------------------
 
 -- SET ECHO ON
-
-PROMPT Disable CDB Plan
-
-ALTER SYSTEM SET resource_manager_plan='ORA$INTERNAL_CDB_PLAN' SCOPE=BOTH;
-
-PROMPT Drop resource plan
-
-BEGIN
-  DBMS_RESOURCE_MANAGER.CREATE_PENDING_AREA();
-  DBMS_RESOURCE_MANAGER.DELETE_CDB_PLAN('jar_cdb_plan');
-  DBMS_RESOURCE_MANAGER.VALIDATE_PENDING_AREA();
-  DBMS_RESOURCE_MANAGER.SUBMIT_PENDING_AREA();
-END;
-/
 
 PROMPT  Create a pending area using the CREATE_PENDING_AREA procedure:
 
