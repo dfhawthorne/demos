3f9d64f0723a2e8c0cdb9292fbca680afec8f5d0
douglas <dfhawthorne@computer.org>
2021-01-15 13:46:20 +1100

    Test Runs on XEN Server (Victoria) Collect more statistics from AWR reports Resolved issue #38 - Save state for PDB Resolved issue #36 - Stop automatic plan switching Resolved issue #39 - Fixed syntax error for setting plan Resolved issue #41 - Stops firewall before running test Automatically generate AWR report after load test Resolved issue #40 - Fixed TCL syntax errors

--- resource_manager_thruput/extract_stats_from_awr_rpt.py
+++ resource_manager_thruput/extract_stats_from_awr_rpt.py
@@ -21,16 +21,25 @@ except ModuleNotFoundError as ex:
 
 import os
 import statistics
+from datetime import datetime
 
 # ------------------------------------------------------------------------------
 # Load data from AWR reports
 # ------------------------------------------------------------------------------
 
-awr_dir          = "./awr_reports/"
-data             = dict()
-data['plan']     = list()
-data['num_cpus'] = list()
-data['rate']     = list()
+awr_dir               = "./awr_reports/"
+data                  = dict()
+data['plan']          = list()
+data['num_cpus']      = list()
+data['rate']          = list()
+data['memory']        = list()
+data['platform']      = list()
+data['logical_reads'] = list()
+data['user_calls']    = list()
+data['SQL_executes']  = list()
+data['dbid']          = list()
+data['startup_time']  = list()
+data['snap_time']     = list()
 
 for file_name in os.listdir(awr_dir):
     if not os.path.isfile(awr_dir + file_name):
@@ -40,14 +49,33 @@ for file_name in os.listdir(awr_dir):
 
     tables = soup.find_all('table')
     for t in tables:
-        if t.get('summary') == "This table displays host information":
-            rows = t.find_all('tr')
-            cols = rows[1].find_all('td')
-            num_cpus = int(cols[2].text)
+        if   t.get('summary') == "This table displays pdb instance information":
+            rows         = t.find_all('tr')
+            cols         = rows[1].find_all('td')
+            dbid         = int(cols[0].text)
+            startup_time = cols[2].text
+        elif t.get('summary') == "This table displays host information":
+            rows         = t.find_all('tr')
+            cols         = rows[1].find_all('td')
+            num_cpus     = int(cols[2].text)
+            platform     = cols[1].text
+            memory       = float(cols[5].text.replace(',',''))
         elif t.get('summary') == "This table displays snapshot information":
-            rows = t.find_all('tr')
-            cols = rows[3].find_all('td')
+            rows         = t.find_all('tr')
+            cols         = rows[2].find_all('td')
+            snap_time    = datetime.strptime(cols[2].text, '%d-%b-%y %H:%M:%S')
+            cols         = rows[3].find_all('td')
             elapsed_time = float(cols[2].text.split()[0]) * 60.0
+        elif t.get('summary') == "This table displays load profile":
+            for r in t.find_all('tr'):
+                cols = r.find_all('td')
+                if   len(cols) >= 2:
+                    if   cols[0].text == "Logical read (blocks):":
+                        logical_reads = float(cols[1].text.replace(',',''))
+                    elif cols[0].text == "User calls:":
+                        user_calls    = float(cols[1].text.replace(',',''))
+                    elif cols[0].text == "Executes (SQL):":
+                        SQL_executes  = float(cols[1].text.replace(',',''))
         elif t.get('summary') == "This table displays top SQL by number of executions":
             for row in t.find_all('tr')[1:]:
                 cols = row.find_all('td')
@@ -65,9 +93,19 @@ for file_name in os.listdir(awr_dir):
                         resource_manager_plan = "INTERNAL_PLAN"
                     break
             if cols[2].text.strip() == '':
+                if resource_manager_plan.startswith("FORCE:"):
+                    resource_manager_plan = resource_manager_plan.replace("FORCE:","")
                 data['plan'].append(resource_manager_plan)
                 data['num_cpus'].append(num_cpus)
                 data['rate'].append(rate)
+                data['memory'].append(memory)
+                data['platform'].append(platform)
+                data['logical_reads'].append(logical_reads)
+                data['user_calls'].append(user_calls)
+                data['SQL_executes'].append(SQL_executes)
+                data['dbid'].append(dbid)
+                data['startup_time'].append(startup_time)
+                data['snap_time'].append(snap_time)
                 print(f"File_name='{file_name}',Plan='{resource_manager_plan}',Num CPUs={num_cpus:2d},rate={rate:.2f}")
             else:
                 print(f"File_name='{file_name}',Plan changed from '{cols[1].text}' to '{cols[2].text}' - ignored")
